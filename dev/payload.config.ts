4408e1f659463c275deec8ff05578820309fcafe
Erik Reiblein <erreib@gmail.com>
2025-03-23 18:37:33 -0400

    enabling default options in dev template for toolbar testing

--- dev/payload.config.ts
+++ dev/payload.config.ts
@@ -67,7 +67,8 @@ export default buildConfig({
     },
   }),
   editor: lexicalEditor({
-    features: [
+    features: ({ defaultFeatures }) => [
+      ...defaultFeatures,
       CommentFeature(),
     ],
   }),
